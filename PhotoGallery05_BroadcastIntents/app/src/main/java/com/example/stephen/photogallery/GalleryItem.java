1731ec2877b15672f546df72ad165015e0fe4ccb
Stephen <stephen@Stephens-MacBook-Pro.local>
2016-06-30 13:18:43 -0400

    PhotoGallery05_BroadcastIntents

---
+++ PhotoGallery05_BroadcastIntents/app/src/main/java/com/example/stephen/photogallery/GalleryItem.java
@@ -0,0 +1,39 @@
+package com.example.stephen.photogallery;
+
+/**
+ * Created by stephen on 16-06-20.
+ */
+public class GalleryItem {
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+
+    public String getCaption() {
+        return mCaption;
+    }
+
+    public void setCaption(String caption) {
+        mCaption = caption;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public void setId(String id) {
+        mId = id;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+
+    @Override
+    public String toString() {
+        return mCaption;
+    }
+}
---
+++ PhotoGallery05_BroadcastIntents/app/src/main/java/com/example/stephen/photogallery/VisibleFragment.java
@@ -0,0 +1,40 @@
+package com.example.stephen.photogallery;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.util.Log;
+
+/**
+ * Created by stephen on 16-06-30.
+ */
+public abstract class VisibleFragment extends Fragment {
+    private static final String TAG = "VisibleFragment";
+
+    private BroadcastReceiver mOnShowNotification = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // If we receive this, we're visible, so cancel
+            // the notification
+            Log.i(TAG, "canceling notification");
+            setResultCode(Activity.RESULT_CANCELED);
+        }
+    };
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        IntentFilter filter = new IntentFilter(PollService.ACTION_SHOW_NOTIFICATION);
+        getActivity().registerReceiver(mOnShowNotification, filter,
+                PollService.PERM_PRIVATE, null);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        getActivity().unregisterReceiver(mOnShowNotification);
+    }
+}
